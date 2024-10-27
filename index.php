a067cd79606a4b1bededeb26839ccee4868eec0e
ivajc <ivajcc@hotmail.com>
2024-10-27 09:53:29 +0100

    init

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Javier Cerejido Cortés</title>
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="logos/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="logos/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="logos/favicon-16x16.png">
+</head>
+<body>
+
+    <div class="main-container">
+        <!-- Columna izquierda: Acerca de mí y Contacto -->
+        <div class="left-column">
+            <!-- Acerca de mí -->
+            <div class="section" id="about">
+                <h2>Acerca de nosotros</h2>
+                <p>ASIR Maria Teresa	Joven Sanagustín</p>
+                <p>DAW Ludmila Da Silva Marques</p>
+                <p>DAM Javier Cerejido Cortés</p>
+            </div>
+
+            <!-- Contacto -->
+            <div class="section" id="contact">
+                <h2>Contacto</h2>
+                <p>📧 t.jov@hotmail.com</p>
+                <p>📧 ludmiladsilva1@gmail.com</p>
+                <p>📧 ivajcc@hotmail.com</p>
+            </div>
+        </div>
+
+        <!-- Proyectos (lado derecho) -->
+        <div class="section" id="projects">
+            <h2>Proyecto Reto Big Data</h2>
+            <!-- Div que contiene los botones de proyectos -->
+            <div class="project-buttons">
+                <h3 class="proyecto-titulo">Entregable Parcial</h3>
+                <a href="docs/seccion1.pdf" class="button" target="_blank">Sección #1</a>
+                <a href="docs/seccion2.pdf" class="button" target="_blank">Sección #2</a>
+                <a href="docs/lab3.pdf" class="button" target="_blank">Sección #3</a>
+                <a href="docs/lab4.pdf" class="button" target="_blank">Sección #4</a>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <p>©grupo 7</p>
+    </footer>
+</body>
+</html>
+
